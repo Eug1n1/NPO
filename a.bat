f5bcb766a7e28345818ad33ab51241b1dc7d71f8
Shumskiy Evgeniy <vkzheny@gmail.com>
2022-11-16 20:00:27 +0300

    a

---
+++ a.bat
@@ -0,0 +1,2 @@
+cd ./lab_7/CalculatorTest/bin/Debug/net6.0
+C:\Users\eug1n1\scoop\apps\nunit-console\current\nunit3-console.exe CalculatorTest.dll
---
+++ npo_5/npo_5/Program.cs
@@ -0,0 +1,28 @@
+﻿string[] args_arr = Environment.GetCommandLineArgs();
+
+if (args_arr.Length == 2)
+{
+    new Exception("Нет строки. почему?");
+}
+
+string str = args_arr[1];
+Console.WriteLine($"str: {str}");
+List<char> list = new List<char>();
+int max = 0;
+
+for (int i = 0; i < str.Length; i++)
+{
+    if (list.Contains(str[i]))
+    {
+        list = new List<char>();
+    }
+
+    list.Add(str[i]);
+
+    if (list.Count() > max)
+    {
+        max = list.Count();
+    }
+}
+
+Console.WriteLine(max);
