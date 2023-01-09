792bbc683788c99018b9706e260c90109ece5694
Shumskiy Evgeniy <vkzheny@gmail.com>
2023-01-09 16:05:09 +0600

    add DriverWait

--- lab_9/lab_9/Program.cs
+++ lab_9/lab_9/Program.cs
@@ -1,34 +1,49 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
 
-Console.WriteLine("Start");
-
-FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\Users\eug1n1\scoop\apps\GeckoDriver\current", "geckodriver.exe"); 
-service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
-
-IWebDriver driver = new FirefoxDriver(service);
-
-driver.Navigate().GoToUrl("https://www.adidas.com/us");
-
-var cookieButton = driver.FindElements(By.ClassName("glass-gdpr-default-consent-accept-button"));
-if (cookieButton.Count > 0)
+internal class Program
 {
-    cookieButton[0].Click();
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Start");
+
+        FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(@"C:\Users\eug1n1\scoop\apps\GeckoDriver\current", "geckodriver.exe");
+        service.FirefoxBinaryPath = @"C:\Program Files\Mozilla Firefox\firefox.exe";
+
+        IWebDriver driver = new FirefoxDriver(service);
+        WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+        driver.Navigate().GoToUrl("https://www.adidas.com/us");
+
+        //var cookieButton = wait.Until(condition => condition.FindElements(By.ClassName("glass-gdpr-default-consent-accept-button")));
+        //if (cookieButton.Count > 0)
+        //{
+        //    cookieButton[0].Click();
+        //}
+
+        IWebElement searchForm = wait.Until(condition => condition.FindElement(By.Name("q")));
+
+        searchForm.SendKeys("superstar");
+        searchForm.Submit();
+
+        //var modalCloseButton = wait.Until(condition => condition.FindElements(By.ClassName("gl-modal__close")));
+        //Thread.Sleep(3000);
+        //if (modalCloseButton.Count > 0)
+        //{
+        //    modalCloseButton[0].Click();
+        //}
+
+        Thread.Sleep(5000);
+
+
+        var products = wait.Until(condition => condition.FindElement(By.XPath("/html/body/div[2]/div/div[1]/div[1]/div/div/div[3]/div/div[3]/div/div/div[2]/div[1]/div/div[1]")));
+
+        Console.WriteLine($"products: {products.Text}");
+
+        products.FindElement(By.TagName("a")).Click();
+
+        Console.ReadKey(true);
+        driver.Quit();
+    }
 }
-
-IWebElement searchForm = driver.FindElement(By.Name("q"));
-searchForm.SendKeys("superstar");
-searchForm.Submit();
-
-Thread.Sleep(5000);
-
-var modalCloseButton = driver.FindElements(By.ClassName("gl-modal__overlay"));
-if (modalCloseButton.Count > 0)
-{
-    modalCloseButton[0].Click();
-}
-
-var products = driver.FindElements(By.ClassName("glass-product-card"));
-Console.WriteLine($"products: {products.Count()}");
-
-products[0].FindElement(By.TagName("a")).Click();
